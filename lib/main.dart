0ed9220adf2791486a4cfae5b5bae50b43e68c3e
Savio Costa <saviocosta@Mac-mini-de-Savio.local>
2025-03-30 23:48:31 -0300

    Alteração para teste de Deploy Automatizado

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Apertou muito o botão já:'),
+            const Text('Botão selecionado muitas vezes:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
